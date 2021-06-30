d61ba8c4730b757cd8bc839a43bc9488f5f0c40e
devchester14 <devchester404@gmail.com>
2021-06-30 16:52:38 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+import axios from 'axios';
+import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+    width: 250,
+  },
+});
+
+const App = () => {
+  const [posts, setPosts] = useState([]);
+  const [pageNumber, setPageNumber] = useState(1);
+  const classes = useStyles();
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const res = await axios.get(
+        `https://reqres.in/api/users?page=${pageNumber}`
+      );
+      setPosts(res.data.data);
+    };
+
+    fetchUsers();
+  }, [pageNumber]);
+
+  console.log(posts);
+
+  const apihandler = () => {
+    setPageNumber(2);
+  };
+  const apihandlerprev = () => {
+    setPageNumber(1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}
+    >
+      {posts.map((post) => {
+        return (
+          <Card key={post.id} className={classes.root}>
+            <CardActionArea>
+              <CardMedia
+                className={classes.media}
+                image={post.avatar}
+                title={post.first_name}
+              />
+              <CardContent>
+                <Typography gutterBottom variant='h5' component='h1'>
+                  {post.first_name} {post.last_name}
+                </Typography>
+                <Typography variant='body2' color='textSecondary' component='p'>
+                  {post.email}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <br />
+          </Card>
+        );
+      })}
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          flexDirection: 'row',
+          justifyItems: 'center',
+        }}
+      >
+        <button onClick={apihandlerprev}>Prev Page</button>
+        <button onClick={apihandler}>NextPage</button>
+      </div>
     </div>
   );
-}
-
+};
 export default App;
